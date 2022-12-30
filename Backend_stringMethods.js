7612859cde8e619004cb5fe94a834bd3e0df6ed0
Sushant <93951120+07Sushant@users.noreply.github.com>
2022-12-30 15:45:50 +0530

    Add files via upload

---
+++ Backend_stringMethods.js
@@ -0,0 +1,27 @@
+let name = 'Sushant'
+upper = name.toUpperCase()
+console.log(upper)
+
+// typeof funciton
+
+console.log(typeof name)
+
+// string slicing
+console.log(name.slice(0,4))
+
+// convert string to number
+name = +name
+// convert number to string
+name = name +""
+// string concatenation
+
+let firstName = 'Sushant'
+let lastname = 'Kumar'
+let fullName = firstName + lastname
+console.log(fullName)
+
+// template strings 
+// this is like formatted strings in python but the syntax is different
+let age = 17
+let aboutMe = `My name is ${name} and my age is ${age}`
+let aboutMe1 = `My name is ${name} and my age is ${age}`
