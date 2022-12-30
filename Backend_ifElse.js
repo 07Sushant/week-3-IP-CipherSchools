19f43bb57824b9ea293a643759c461c4b8e64a83
Sushant <93951120+07Sushant@users.noreply.github.com>
2022-12-30 15:45:59 +0530

    Add files via upload

---
+++ Backend_ifElse.js
@@ -0,0 +1,30 @@
+let age = 16;
+if (age>=18){
+    console.log("you can play GTA")
+
+}
+else{
+    console.log("Sorry u can't")
+}
+// ternary operator
+let age2 = 5;
+let canDrink = (age>=21) ? "Alcohol":"Milk" // condition ? value1:value2
+console.log(canDrink)
+
+let age3 = 17;
+let name = "Sushant"
+// and
+if (name[0] == "S" && age<=17){
+    console.log("Minor")
+}
+else{
+    console.log("Major")
+}
+
+//or
+if (name[0] == "S" || age<=17){
+    console.log("Minor")
+}
+else{
+    console.log("Major")
+}
